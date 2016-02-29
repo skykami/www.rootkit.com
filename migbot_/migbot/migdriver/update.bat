260bcfef76450b7ec23f59376652c0bb32dfc66e
fdiskyou <fdiskyou@users.noreply.github.com>
2016-02-29 09:44:29 +0000

    sync

--- migbot_/migbot/migdriver/update.bat
+++
@@ -1,6 +0,0 @@
-build
-copy c:\vice\migbot\migdriver\objchk_w2k\i386\MIGBOT.sys c:\vice\migbot\migloader\
-copy c:\vice\migbot\migdriver\objchk_w2k\i386\MIGBOT.pdb \\10.9.8.106\c$
-copy c:\vice\migbot\migdriver\objchk_w2k\i386\MIGBOT.sys \\10.9.8.106\c$
-copy c:\vice\migbot\migdriver\migsys.c \\10.9.8.106\c$
-
